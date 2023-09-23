e2c028199b2aff9737bb5d744c94ee4554f27034
Mutoy-choi <urisem625@gmail.com>
2023-09-23 23:18:19 +0900

    Update one_shot_test_ParallelUnet.py

--- one_shot_test_ParallelUnet.py
+++ one_shot_test_ParallelUnet.py
@@ -207,27 +207,15 @@ parallel_config_256 = {
 # Initialize both models and their optimizers
 model1 = ParallelUNet(EMB_DIM, parallel_config_128)
 model2 = ParallelUNet(EMB_DIM, parallel_config_256)  # Assuming you have a ParallelUNet class
-model3 = EfficientUNet()
 
 optimizer1 = optim.AdamW(model1.parameters(), lr=0.0001)
 optimizer2 = optim.AdamW(model2.parameters(), lr=0.0001)
-optimizer3 = optim.AdamW(model3.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-8, weight_decay=0)
-
-# EfficientUNet에 대한 CosineAnnealing 스케줄러
-T_max = 2500000  # 학습 에포크 수에 따라 변하는 하이퍼 파라미터 값입니다.
-scheduler3 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer3, T_max=T_max)
-
-# EfficientUNet에 대한 Linear warmup 스케줄러
-warmup_steps = 10000
-lr_lambda = lambda epoch: min(1.0, epoch / warmup_steps)
-warmup_scheduler3 = torch.optim.lr_scheduler.LambdaLR(optimizer3, lr_lambda)
 
 criterion = torch.nn.MSELoss()  # Mean Squared Error Loss
 
 # Move both models to the GPU
 model1.to(device)
 model2.to(device)
-model3.to(device)
 
 # Initialize the gradient scaler for fp16
 scaler = GradScaler()
@@ -245,7 +233,6 @@ for epoch in range(10):  # 10 epochs
 
     optimizer1.zero_grad(set_to_none=True)
     optimizer2.zero_grad(set_to_none=True)
-    optimizer3.zero_grad(set_to_none=True)
 
     # Wrap your dataloader with tqdm for a progress bar
     for i, (combined_img, person_pose, garment_pose, ic_img, org_img) in enumerate(dataloader):
@@ -270,12 +257,8 @@ for epoch in range(10):  # 10 epochs
             output2 = model2(output1, person_pose, garment_pose, ic_img)  # Using output1 as input to model2
             loss2 = criterion(output2, org_img)
 
-            # Model 3's forward pass and loss calculation
-            output3 = model3(output2) # Using output2 as input to model3
-            loss3 = criterion(output3, org_img)
-
             # Combine the losses if needed
-            loss = loss1 + loss2 + loss3
+            loss = loss1 + loss2
             loss = loss / accumulation_steps  # Normalize the loss because it is accumulated
 
         # Backpropagation using gradient accumulation
@@ -285,11 +268,11 @@ for epoch in range(10):  # 10 epochs
             print(f"Epoch {epoch + 1}, Batch {i + 1}, Loss: {loss.item() * accumulation_steps}")
             scaler.step(optimizer1)  # Performs the optimizer step for model1
             scaler.step(optimizer2)  # Performs the optimizer step for model2
-            scaler.step(optimizer3)
+
             scaler.update()  # Updates the scale for next iteration
             optimizer1.zero_grad()  # Reset gradients tensors for model1
             optimizer2.zero_grad()  # Reset gradients tensors for model2
-            optimizer3.zero_grad()
+
 
         epoch_loss += loss.item() * accumulation_steps  # Accumulate the true loss
         num_batches += 1
@@ -302,15 +285,10 @@ for epoch in range(10):  # 10 epochs
     elapsed_time = epoch_end_time - epoch_start_time
     print(f"Time taken for Epoch {epoch + 1}: {elapsed_time:.2f} seconds")
 
-    # This scheduler is specifically applied to EfficientUNet
-    scheduler3.step()
-    warmup_scheduler3.step()
-
     # Save the models
     if epoch % 2 == 0:
         torch.save(model1.state_dict(), f"lightweight_parallel_unet_model1_epoch_{epoch + 1}.pt")
         torch.save(model2.state_dict(), f"parallel_unet_model2_epoch_{epoch + 1}.pt")
-        torch.save(model3.state_dict(), f"efficient_unet_model3_epoch_{epoch + 1}.pt")
         print("Models saved.")
 
 
